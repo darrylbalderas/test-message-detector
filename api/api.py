472602b146f818814392b9ddafa22410e5fce3cb
Darryl-Balderas <darryl.balderas@ibm.com>
2019-03-19 20:16:36 -0700

    Clean up front end to add flash messaging
    Signed-off-by: Darryl-Balderas <darryl.balderas@ibm.com>

--- api/api.py
+++ api/api.py
@@ -1,5 +1,8 @@
-from flask import Flask, redirect, url_for, render_template, request
-app = Flask(__name__)
+from flask import Flask, redirect, url_for, render_template, request, flash
+
+app = Flask(__name__, static_url_path='/static')
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 
 
 @app.route('/', methods=['GET'])
@@ -10,27 +13,11 @@ def homepage():
 @app.route('/verifyMessage', methods=['POST'])
 def check_message():
     if request.method == 'POST':
-        if request.form['title'] == 'wca':
-            return redirect(url_for('message_status', status="test"))
-        return redirect(url_for('message_status', status="notest"))
+        title = request.form['title']
+        subtitle = request.form['subtitle']
+        message = request.form['message']
+        flash("not test message")
     return redirect(url_for('homepage'))
-
-
-@app.route('/messageStatus/<status>', methods=['GET'])
-def message_status(status):
-    if status == "test":
-        return redirect(url_for('test'))
-    return redirect(url_for('no_test'))
-
-
-@app.route('/test')
-def test():
-    return 'Test message'
-
-
-@app.route('/notest')
-def no_test():
-    return 'Not a test message'
 
 
 if __name__ == '__main__':
